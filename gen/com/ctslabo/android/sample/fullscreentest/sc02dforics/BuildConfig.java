f7c09ed713aaf194eeaeb130e147d89b7c5306e8
ctslabo <ctslabo@gmail.com>
2013-03-29 21:11:59 +0900

    deleted

--- gen/com/ctslabo/android/sample/fullscreentest/sc02dforics/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.ctslabo.android.sample.fullscreentest.sc02dforics;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
